d57666e77741824aad23b10c8987832e2482212e
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-03-26 14:29:37 +0530

    All UI part one

--- frontend/src/components/Layout/constants/Color.js
+++ frontend/src/components/Layout/constants/Color.js
@@ -2,3 +2,5 @@ export const Orange = "#ea7070";
 export const GrayColor = "rgba(247,247,247,1)";
 export const SenderColor = "#2694ab";
 export const MatBlack = "#1c1c1c";
+export const LinearGradient =
+  "linear-gradient(rgb(255,225,209),rgb(249,159,159))";
